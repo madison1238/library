014c873df460b97f521cad305d1baabe8de0d6a7
madison mirelez <madis123879@gmail.com>
2025-07-03 20:07:55 -0500

    feat: create functions to display books in library on cards

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-const library = document.querySelector('#library')
-const newDiv = document.createElement('div')
+const libraryCont = document.querySelector('#library')
 
 
 const myLibrary = [];
@@ -19,19 +18,55 @@ function addBookToLibrary(title,author,pages,read){
 }
 
 function createCard(book){
-    const newDiv = Document.createElement('div')
-    newDiv.classList.add('card')
-    library.appendChild(newDiv);
+    const newCard = document.createElement('div');
+    newCard.classList.add('card');
 
+    const title = document.createElement('p');
+    title.id = 'title';
+    title.textContent = `${book.title}`;
+
+    const author = document.createElement('p');
+    author.id = 'author';
+    author.textContent = `${book.author}`;
+
+    const pages = document.createElement('p');
+    pages.id = 'pages';
+    pages.textContent = `${book.pages} pages`;
+
+    const read = document.createElement('button');
+    read.id = 'read'
+    if(book.read === true){
+        read.textContent = 'read';
+        read.classList.add('yread');
+    }
+    else{
+        read.textContent = 'not read';
+        read.classList.add('nread');
+    }
+
+    const remove = document.createElement('button');
+    remove.id = 'remove';
+    remove.textContent = 'remove'
+    
+    libraryCont.appendChild(newCard);
+    newCard.appendChild(title);
+    newCard.appendChild(author);
+    newCard.appendChild(pages);
+    newCard.appendChild(read)
+    newCard.appendChild(remove);
 }
 
 function displayBooks(library){
+    libraryCont.innerHTML = '';
     for(let book of library ){
-        createCard(book)
+        createCard(book);
     }
 }
 
 
 
 addBookToLibrary('mad', 'madi', 32, true)
+displayBooks(myLibrary)
+addBookToLibrary('korn', 'fred', 543, false)
+displayBooks(myLibrary)
 
