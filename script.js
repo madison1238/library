e830cfea1e09fee789e2821ac126fba0656c5327
madison mirelez <madis123879@gmail.com>
2025-07-15 18:05:42 -0500

    refactor: change constructor function to class

--- script.js
+++ script.js
@@ -8,13 +8,17 @@ const readBtn = document.querySelectorAll('#read')
 
 const myLibrary = [];
 
-function Book(title, author, pages, read){
+
+class Book {
+    constructor(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.id = crypto.randomUUID();
+    }
 }
+
 
 function addBookToLibrary(title,author,pages,read){
     newBook = new Book(title,author,pages,read);
